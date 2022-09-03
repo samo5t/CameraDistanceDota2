b33db1133b537c07e4b35e960fe181ec1e68ad13
samo5t <81762548+samo5t@users.noreply.github.com>
2022-09-03 23:55:16 +0300

    Add files via upload

---
+++ Start.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'CameraDistanceDota2.exe' with Python DLL being found.
+set PATH=c:\users\abuse\appdata\local\programs\python\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Users\abuse\AppData\Local\Programs\Python\Python310
+"%~dp0.\CameraDistanceDota2.exe" %*
