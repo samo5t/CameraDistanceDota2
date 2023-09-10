a4ce4f07a18c110315374560f16b3b4d2d43ba24
samo5t <81762548+samo5t@users.noreply.github.com>
2023-09-10 21:35:34 +0300

    Update CameraDistanceDota2.py

--- CameraDistanceDota2.py
+++ CameraDistanceDota2.py
@@ -1,57 +1,22 @@
-import glob
 import os
-import pathlib
 import time
-import subprocess
-from pathlib import Path
 import configparser
+import sys
+
 os.system("mode con cols=115 lines=30")
 config = configparser.ConfigParser()
 config.read("settings.ini")
 d = config["path"]["directory"]
+print('╔═══╗╔═══╗╔═══╗\n'
+      '╚╗╔╗║║╔═╗║║╔══╝\n'
+'─║║║║║║─╚╝║╚══╗\n'
+'─║║║║║║─╔╗║╔══╝\n'
+'╔╝╚╝║║╚═╝║║║───\n'
+'╚═══╝╚═══╝╚╝───\n')
 print(config["path"]["directory"])
-targetPattern = r"\**\steamapps\common\dota 2 beta\game\dota\bin\win64\client.dll"
-
-s = (b'\x00\x40\x9C\x44\x48\x8D\x15\x4B', #1250
-     b'\x00\x80\xA2\x44\x48\x8D\x15\x4B',
-     b'\x00\xC0\xA8\x44\x48\x8D\x15\x4B',
-     b'\x00\x00\xAF\x44\x48\x8D\x15\x4B',
-     b'\x00\x40\xB5\x44\x48\x8D\x15\x4B',
-     b'\x00\x80\xBB\x44\x48\x8D\x15\x4B',
-     b'\x00\xC0\xC1\x44\x48\x8D\x15\x4B',
-     b'\x00\x00\xC8\x44\x48\x8D\x15\x4B',
-     b'\x00\x40\xCE\x44\x48\x8D\x15\x4B',
-     b'\x00\x80\xD4\x44\x48\x8D\x15\x4B',
-     b'\x00\xC0\xDA\x44\x48\x8D\x15\x4B',
-     b'\x00\x00\xE1\x44\x48\x8D\x15\x4B',
-     b'\x00\x40\xE7\x44\x48\x8D\x15\x4B',
-     b'\x00\x80\xED\x44\x48\x8D\x15\x4B',
-     b'\x00\xC0\xF3\x44\x48\x8D\x15\x4B',
-     b'\x00\x00\xFA\x44\x48\x8D\x15\x4B')
-
-def redist(disk,j):
-    with open(disk, 'rb') as file:
-        data = file.read()
-    data = bytearray(data)
-
-    data = data.replace(b'\x00\x00\x96\x44\x00\x00\xE1\x44',s[j])
-
-    with open(disk, 'wb') as file:
-        file.write(data)
-
-def out_green(text):
-    print("\033[32m {}" .format(text))
-
-def out_red(text):
-    print("\033[31m {}" .format(text))
-
-print(config["path"]["directory"])
-startFile = (d[:d.find("\common")] + r'\common\dota 2 beta\game\bin\win64\dota2.exe','-c')
-print(startFile)
 print("Для успешного изменения необходимо, чтобы у вас был не модифицированный файл Client.dll\n" 
       "Используйте ваш путь к игре в файле settings.ini /**/steamapps/common/dota 2 beta/game/dota/bin/win64/\n" 
-      "(C)cmct_")
-
+      "(C)cmct_\n")
 print('Выберете номер в списке соответсвующей желаемому значению\n'
           ' 1) 1250\n'
           ' 2) 1300\n'
@@ -69,23 +34,73 @@ print('Выберете номер в списке соответсвующей 
           '14) 1900\n'
           '15) 1950\n'
           '16) 2000\n')
+s = (b'\x00\x40\x9C\x44',
+     b'\x00\x80\xA2\x44',
+     b'\x00\xC0\xA8\x44',
+     b'\x00\x00\xAF\x44',
+     b'\x00\x40\xB5\x44',
+     b'\x00\x80\xBB\x44',
+     b'\x00\xC0\xC1\x44',
+     b'\x00\x00\xC8\x44',
+     b'\x00\x40\xCE\x44',
+     b'\x00\x80\xD4\x44',
+     b'\x00\xC0\xDA\x44',
+     b'\x00\x00\xE1\x44',
+     b'\x00\x40\xE7\x44',
+     b'\x00\x80\xED\x44',
+     b'\x00\xC0\xF3\x44',
+     b'\x00\x00\xFA\x44')
+def redist(disk,j):
+    with open(disk, 'rb') as file:
+        data = file.read()
+    data = bytearray(data)
 
-if int(config["path"]["distance"]) >= 1 and int(config["path"]["distance"]) <=16:
-    try:
-        redist(config["path"]["directory"], int(config["path"]["distance"])-1)
-        out_green("Успешно\n Значение считано из файла, окно закроется через 1 секунду и запустится Dota")
-        subprocess.Popen(startFile)
-        time.sleep(1)
-    except Exception:
-        out_red('Неверный путь')
-        time.sleep(5)
+    # Находим пятую позицию для замены
+    fifth_occurrence = 0
+    position = -1
+
+    while fifth_occurrence < 5:
+        position = data.find(b'\x00\x00\x96\x44', position + 1)
+        if position == -1:
+            break
+        fifth_occurrence += 1
+
+    if fifth_occurrence == 5 and position != -1:
+        # Заменяем пятое вхождение
+        data[position:position + 4] = s[j]
+
+    with open(disk, 'wb') as file:
+        file.write(data)
+
+
+def loading_bar(duration):
+    total_width = 40
+    start_time = time.time()
+    end_time = start_time + duration
+
+    while time.time() < end_time:
+        fraction = (time.time() - start_time) / duration
+        width = int(total_width * fraction)
+        bar = '[' + '=' * width + ' ' * (total_width - width) + ']'
+        sys.stdout.write('\r' + bar)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+def out_green(text):
+    print("\033[32m {}" .format(text))
+
+def out_red(text):
+    print("\033[31m {}" .format(text))
+
+
+a = input()
+
+try:
+    redist(config["path"]["directory"], int(a) - 1)
+except Exception:
+    out_red('Неверное значение или путь')
 else:
-
-    a = input()
-
-    try:
-        redist(config["path"]["directory"], int(a) - 1)
-    except Exception:
-            out_red('Неверное значение или путь')
-    else:
-            out_green("Успешно")
+    out_green("Успешно\n")
+print('Закрытие через 3 секунды\n')
+loading_duration = 3
+loading_bar(loading_duration)
